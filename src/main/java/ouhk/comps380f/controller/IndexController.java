0738e1c99b6710c1ba71f560e5ac3ced1b64fb88
Ben <57297093+Benwyw@users.noreply.github.com>
2021-04-17 03:08:28 +0800

    JM9

--- src/main/java/ouhk/comps380f/controller/IndexController.java
+++ src/main/java/ouhk/comps380f/controller/IndexController.java
@@ -14,6 +14,7 @@ public class IndexController {
         return "index";
         //return "redirect:/ticket/list";
     }
+<<<<<<< HEAD
     
     @GetMapping(value = "/login")
     public String login() {
@@ -24,4 +25,6 @@ public class IndexController {
         return "logout";
     }
     
+=======
+>>>>>>> parent of 393b34c (login卡關 (有bug))
 }
